31f64184ae4512ff5723068275dd83a2e42a33c3
Burcin Karimov <burcinkarimov@gmail.com>
2024-02-28 09:56:54 +0400

    Remove type submit from PrimaryButton

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,6 +1,6 @@
 function PrimaryButton({ text }) {
   return(
-    <button type="submit" className="px-4 py-2 w-20 border-[1px] bg-indigo-100 hover:bg-indigo-300">{text}</button>
+    <button className="px-4 py-2 w-20 border-[1px] bg-indigo-100 hover:bg-indigo-300">{text}</button>
   )
 }
 
