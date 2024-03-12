44914b52d62cfe6f0b12906e2292bb94b3415633
Burcin Karimov <burcinkarimov@gmail.com>
2024-03-12 16:35:57 +0400

    Add delete button for tasks

--- src/components/TasksListItem.js
+++ src/components/TasksListItem.js
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faCheck, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function TasksListItem({ task }) {
   return (
     <li className="p-2 border-[1px] my-2 flex items-center">
       <FontAwesomeIcon icon={faCheck} className="p-2 mr-4 border-[1px] text-amber-200 hover:text-amber-400 cursor-pointer" />
       <p>{task}</p>
-      <FontAwesomeIcon icon={faPenToSquare} className="ml-auto p-2 border-[1px] text-indigo-400 hover:bg-indigo-600 hover:text-white cursor-pointer" />
+      <div className="flex gap-1 ml-auto">
+        <FontAwesomeIcon icon={faPenToSquare} className="p-2 border-[1px] text-indigo-400 hover:bg-indigo-500 hover:text-white cursor-pointer" />
+        <FontAwesomeIcon icon={faTrash} className="p-2 border-[1px] text-indigo-400 hover:bg-indigo-500 hover:text-white cursor-pointer" />
+      </div>
     </li>
   );
 }
